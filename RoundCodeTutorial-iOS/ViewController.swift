16135a38f1afd62660bf1f7c36a8ef9dbb445185
hyun99999 <ypsophila97@icloud.com>
2022-05-26 23:39:51 +0900

    feat: create coder, encode and set layout

--- RoundCodeTutorial-iOS/ViewController.swift
+++ RoundCodeTutorial-iOS/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+import RoundCode
+import Then
+
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    private let imageView = UIImageView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setCode()
+        setLayout()
     }
-
-
 }
 
+// MARK: - Extensions
+
+extension ViewController {
+    private func setCode() {
+        let image = RCImage(message: "Hello, i am hyungyu")
+        let coder = RCCoder()
+        
+        // MARK: - Error Handling
+        
+        do {
+            imageView.image = try coder.encode(image)
+        } catch {
+            // handle errors
+        }
+    }
+}
+
+// MARK: - Layout
+    
+extension ViewController {
+    private func setLayout() {
+        self.view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 300),
+            imageView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+}
